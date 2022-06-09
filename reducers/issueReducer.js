25c471d4b159e076942791cf3fc7edbcfad92f21
Caelan <caelan.curry@gmail.com>
2022-06-09 22:42:18 +0200

    Implemented issues reducer with state initialisation from DB

--- reducers/issueReducer.js
+++ reducers/issueReducer.js
@@ -1,5 +1,7 @@
 export const issueReducer = (state, action) => {
   switch (action.type) {
+    case "LOAD_ISSUES":
+      return action.payload;
     case "ADD_ISSUE":
       return "issue added";
     case "REMOVE_ISSUE":
